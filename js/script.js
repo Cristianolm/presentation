cc94c9b1202d601b35d08ee84dd88199d663003a
Cristiano Martins <cristianomartins96@hotmail.com>
2022-03-28 05:21:21 +0100

    Update portfolio footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 var menuIcon = document.querySelector("#menu-icon");
+var footerFixed = document.querySelector("#fixed-bottom");
+console.log(footerFixed);
+
+// heigh of client page
+var body = document.body,
+  html = document.documentElement;
+
+var height = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
+
+// END - heigh of client page
 
 menuIcon.addEventListener("click", function () {
   var hasClass = menuIcon.classList.contains("active");
