0648f975e9935d76d204e88b70d2e16545465a04
Cristiano Martins <cristianomartins96@hotmail.com>
2022-07-09 05:09:15 +0100

    Creating potfolio object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+/*
+==========================
+SITE INFORMATION
+==========================
+*/
+const projects = [
+  {
+    id: 0,
+    title: "Ableton",
+    desc: [
+      "<h4>Ableton about page</h4> <p>This project is a clone of the Ableton about page.</p>",
+      "<p> The construction of this page is the result of one of the challenges proposed by the website <a href='https://www.frontendpractice.com/projects/ableton' >Frontend Practice</a >. </p>",
+    ],
+    btns: [
+      ["Clone", "portfolio/Ableton/about.html"],
+      ["Original", "https://www.ableton.com/en/about/"],
+    ],
+  },
+];
+/*
+==========================
+*/
+
 // NAVBAR (mobile)
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
@@ -19,8 +42,3 @@ const myAge = getAge("1996-01-21");
 age.forEach(function (e) {
   e.textContent = myAge;
 });
-
-// Main margin to header
-// const headerHeight = document.querySelector(".header").offsetHeight;
-// const main = (document.querySelector(".main").style.paddingTop =
-//   headerHeight + "px");
