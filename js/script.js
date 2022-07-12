fd024cc033007521c72f6aac9e818232f25be2a2
Cristiano Martins <cristianomartins96@hotmail.com>
2022-07-12 21:12:32 +0100

    Working on buttons arrays

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 /*
-==========================
+========================================================================================================
 SITE INFORMATION
-==========================
+
+
+Note: The values in "tag" can not contain numbers. Beacuse the link between 
+the filter buttons and the projects is connected between the name that appears 
+in the "tag" field and its respective index. But if the value of the "tag" is the value 
+that appears as a display on the site. Hence, in the "displayMenuButtons()" function, 
+there is a pattern that eliminates all values associated with the "tag" field.
+========================================================================================================
 */
 const projects = [
   {
@@ -12,7 +19,7 @@ const projects = [
     ],
     link: "portfolio/Ableton/about.html",
     target: "_blank",
-    tag: ["Cloned Website"],
+    tag: "Cloned Website",
   },
   {
     id: 1,
@@ -22,29 +29,18 @@ const projects = [
     ],
     link: "portfolio/FirstPresentationSite/index.html",
     target: "_blank",
-    tag: ["Personal Website"],
+    tag: "Personal Website",
   },
-  // {
-  //   id: 2,
-  //   title: "For Layout Test",
-  //   desc: [
-  //     "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi perspiciatis optio ratione natus, quos architecto in sed tempore pariatur, assumenda dolor excepturi, est labore doloremque possimus repellat sequi illo recusandae.</p>",
-  //   ],
-  //   link: "#",
-  //   target: "_self",
-  //   tag: ["Cloned Website"],
-  // },
-
-  // {
-  //   id: 3,
-  //   title: "For Layout Test",
-  //   desc: [
-  //     "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi perspiciatis optio ratione natus, quos architecto in sed tempore pariatur, assumenda dolor excepturi, est labore doloremque possimus repellat sequi illo recusandae.</p>",
-  //   ],
-  //   link: "#",
-  //   target: "_self",
-  //   tag: ["Cloned Website"],
-  // },
+  {
+    id: 2,
+    title: "Teste",
+    desc: [
+      "<p>The page you're looking at hasn't always been like this. In this project you can see how I created my first presentation page as a web programmer.</p>",
+    ],
+    link: "portfolio/FirstPresentationSite/index.html",
+    target: "_blank",
+    tag: "Personal Website",
+  },
 ];
 /*
 ==========================
@@ -77,6 +73,76 @@ const portfolioProjects = document.querySelector(".projects");
 
 window.addEventListener("DOMContentLoaded", function () {
   displayPortfolioProjects(projects);
+  displayMenuButtons();
+});
+
+function displayMenuButtons() {
+  let tags = projects.reduce(
+    function (acc, currItem) {
+      acc += currItem.tag;
+      return acc + currItem.id + "-";
+    },
+    ["All-"]
+  );
+
+  var tagsList = tags.split("-");
+  tagsList.pop();
+
+  // Prevent duplication by removing repeted terms and whitespaces
+  cleanTagList = tagsList.map(function (e) {
+    a = e.replace(/^\s\s*/, "").replace(/\s\s*$/, "");
+    return a;
+  });
+
+  // Create two array, where the index values of one "uniqueTagListId" is the correspondent "id" value of the elements in the array "uniqueTagList"
+  var uniqueTagListId = [];
+
+  for (let pos = 1; pos < cleanTagList.length; pos++) {
+    uniqueTagListId[pos] = [cleanTagList[pos].substr(-1)];
+  }
+
+  const uniqueTagList = [...new Set(cleanTagList)];
+
+  for (let pos = 1; pos < uniqueTagList.length; pos++) {
+    var deleteLastNumbers = uniqueTagList[pos].replace(/[0-9]/g, "");
+
+    if (pos !== "-1") {
+      uniqueTagList[pos] = deleteLastNumbers;
+    }
+  }
+  console.log(uniqueTagListId);
+  console.log(uniqueTagList);
+
+  // Generate buttons
+  buttonStr = uniqueTagList.map(function (e) {
+    return `<button class="filter-btn" data-group="${e}">${e}</button>`;
+  });
+  const stringWithAllButtons = buttonStr.join("");
+  let filterProjects = document.querySelector(".filterProjects");
+  filterProjects.innerHTML = stringWithAllButtons;
+
+  // RETURN
+  return uniqueTagList;
+}
+
+function displayPortfolioProjects(portfolioItems) {
+  // displayMenuButtons();
+  let projects = portfolioItems.map(function name(proj) {
+    return `<article data-group=${proj.tag}>
+              <!-- Project title -->
+              <div class="title">
+                <h3><a href="${proj.link}" target = "${proj.target}">${proj.title}</a></h3>
+                <div class="desc-toggle"></div>
+              </div>
+              <!-- Project text -->
+              <div class="project-desc">
+                ${proj.desc}
+              </div>
+            </article>`;
+  });
+
+  stringWithAllProject = projects.join("");
+  portfolioProjects.innerHTML = stringWithAllProject;
 
   // Display Portfolio Description
   const descToggle = document.querySelectorAll(".desc-toggle");
@@ -98,26 +164,6 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-});
-
-function displayPortfolioProjects(portfolioItems) {
-  let projects = portfolioItems.map(function name(proj) {
-    console.log(portfolioItems);
-    return `<article>
-              <!-- Project title -->
-              <div class="title">
-                <h3><a href="${proj.link}" target = "${proj.target}">${proj.title}</a></h3>
-                <div class="desc-toggle"></div>
-              </div>
-              <!-- Project text -->
-              <div class="project-desc">
-                ${proj.desc}
-              </div>
-            </article>`;
-  });
-
-  stringWithAllProject = projects.join("");
-  portfolioProjects.innerHTML = stringWithAllProject;
 }
 
 // Redirect to Correct CV
